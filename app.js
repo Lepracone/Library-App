feec59f40355d24ed6eb177258c6ce83ddfec2cd
Ricardo Ferreira <ricardo199221@gmail.com>
2020-03-16 17:37:22 +0000

    Checkpoint before to try to implement new way to pass form values

--- app.js
+++ app.js
@@ -3,30 +3,24 @@ let myLibrary = [];
 let showNew = document.getElementById("showNew");
 let newBook = document.getElementById("newBook");
 let cancelButton = document.getElementById("cancelButton");
+let isRead = document.getElementById("isRead");
+let isReadValue = false;
 
-
+//Object constructor structure
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;          
-}
+    this.read = read;
+    myLibrary.push(this);         
+};
 
 Book.prototype.info = function(){
-    let readStatus = null;
-            if(this.read == true){
-                readStatus = "already read."
-            }else{
-                readStatus = "not read yet."
-            }
+    const readStatus = this.read ? "read" : "not read yet";
             return (this.title + " by " + this.author + ", " + this.pages + ", " + readStatus);
-}
+};
 
-Book.prototype.addBookToLibrary = function(){
-    myLibrary.push(this);
-}
-
-
+//Toggles hidden menu
 showNew.addEventListener("click", function(){
     toggleClasses();
 });
@@ -39,5 +33,19 @@ function toggleClasses(){
     newBook.classList.toggle("invisible");
     showNew.classList.toggle("invisible");
     cancelButton.classList.toggle("invisible");
-}
+};
+
+//isRead logic
+isRead.addEventListener("click", function(){
+    isReadValue = !isReadValue;
+    isRead.textContent = isReadValue ? "Read": "Unread";
+});
+
+//Creates new object
+function createNewObject(){
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let addedBook = new Book(title, author, pages, isReadValue);
+};
 
