8593584d58cfa8225e92339c57acb0910af75e9b
Ricardo Ferreira <ricardo199221@gmail.com>
2020-03-17 10:30:56 +0000

    Initial render-function implementation

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-let myLibrary = [];
-
 let showNew = document.getElementById("showNew");
 let newBook = document.getElementById("newBook");
 let cancelButton = document.getElementById("cancelButton");
 let isRead = document.getElementById("isRead");
 let isReadValue = false;
 
+let myLibrary = [];
+
+//Prevent page refresh after submit form
+let form = document.getElementById("bookForm");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
 //Object constructor structure
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    myLibrary.push(this);         
+    this.read = read;         
 };
 
 Book.prototype.info = function(){
@@ -47,5 +51,19 @@ function createNewObject(){
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
     let addedBook = new Book(title, author, pages, isReadValue);
+    myLibrary.push(addedBook);
 };
+
+function render(){
+    const cardsContainer = document.getElementById("cardsContainer");
+
+    const frame = document.createElement("div");
+    const picture = document.createElement("img");
+    const textContainer = document.createElement("div");
+
+    const bookTitle = document.createElement("h4");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookRead = document.createElement("p");
+}
 
