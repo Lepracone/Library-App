1bd57cdc74577fe683fda2f6e33133e8d8559f8f
Ricardo Ferreira <ricardo199221@gmail.com>
2020-03-17 10:53:55 +0000

    Add render function

--- app.js
+++ app.js
@@ -52,18 +52,44 @@ function createNewObject(){
     let pages = document.getElementById("pages").value;
     let addedBook = new Book(title, author, pages, isReadValue);
     myLibrary.push(addedBook);
+    //Renders new cards container
+    cardsContainer.textContent = "";
+    render();
 };
 
 function render(){
+    //Create Elements
     const cardsContainer = document.getElementById("cardsContainer");
 
-    const frame = document.createElement("div");
-    const picture = document.createElement("img");
-    const textContainer = document.createElement("div");
+    for(i=0; i < myLibrary.length; i++){
+        const frame = document.createElement("div");
+        const picture = document.createElement("img");
+        const textContainer = document.createElement("div");
 
-    const bookTitle = document.createElement("h4");
-    const bookAuthor = document.createElement("p");
-    const bookPages = document.createElement("p");
-    const bookRead = document.createElement("p");
+        const bookTitle = document.createElement("h4");
+        const bookAuthor = document.createElement("p");
+        const bookPages = document.createElement("p");
+        const bookRead = document.createElement("p");
+
+        //Appends Elements inside the new card frame
+        frame.appendChild(picture);
+        textContainer.appendChild(bookTitle);
+        textContainer.appendChild(bookAuthor);
+        textContainer.appendChild(bookPages);
+        textContainer.appendChild(bookRead);
+        frame.appendChild(textContainer);
+        //Appends the new frame inside the main cardsContainer
+        cardsContainer.appendChild(frame);
+
+        //Adds cardStyle class to frame
+        frame.classList.add("cardStyle");
+
+        //Fills the fields
+        bookTitle.textContent =  myLibrary[i].title;
+        bookAuthor.textContent = myLibrary[i].author;
+        bookPages.textContent = myLibrary[i].pages;
+        bookRead.textContent = myLibrary[i].readStatus;
+    }
 }
+
 
